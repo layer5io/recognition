a6028e444293b15892739aedd6a70419359b6c1d
saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>
2025-12-16 23:44:25 +0530

    added fallback
    Signed-off-by: saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  pathPrefix: process.env.PATH_PREFIX,
+  pathPrefix: process.env.PATH_PREFIX || "/recognition",
   siteMetadata: {
     title: `Layer5 Recognition Program`,
     description: `Showcasing Your Achievements as a User and a Contributor`,
