dbdebe551e5b0e92755b050803810a34472fc838
DANIEL KATOTO <katotodan@gmail.com>
2026-02-09 15:39:04 +0300

    Update Accordion to Sistent Accordion
    Signed-off-by: DANIEL KATOTO <katotodan@gmail.com>

--- src/sitecomponents/Faq/faqSection.style.js
+++ src/sitecomponents/Faq/faqSection.style.js
@@ -10,80 +10,116 @@ const FaqSectionWrapper = styled.section`
     font-style: italic;
     text-transform: capitalize;
   }
-  .accordion__item + .accordion__item {
-    border-color: transparent;
+
+  /* Accordion wrapper */
+  .MuiAccordion-root {
+    margin-bottom: 2px;
+    background: ${(props) => props.theme.body};
+    color: ${(props) => props.theme.text};
+    // border-radius: 8px;
+    overflow: hidden;
+    
+
+    &::before {
+      display: none;
+    }
+    &.Mui-expanded {
+      margin: 0;   /* removes the empty top space */
+    }
+    &.Mui-expanded::before {
+      display: none;
+    }
   }
-  .reusecore__accordion {
-    margin-bottom: 1rem;
+
+  /* Accordion header */
+  .MuiAccordionSummary-root {
+    background: #00b39f;
+    padding: 0rem 1rem;
+
+    h5 {
+      font-weight: 700;
+      font-size: 18px;
+      position: relative;
+      margin: 0;
+      color: #ffffff;
+    }
+    &.Mui-expanded {
+      background: black;
+      border: 1px solid #00b39f;
+    }
   }
-  .accordion__item {
-    .accordion__header {
-      padding:0.5px;
-      background: #00b39f;
-      h5 {
-        font-weight: 700;
-        font-size: 18px;
-        position: relative;
-        color: #ffffff;
-      }
-      > div {
-        &:focus {
-          outline: none;
-        }
-      }
+
+  /* Expand icon */
+  .MuiAccordionSummary-expandIconWrapper {
+    color: #ffffff;
+  }
+
+  /* Accordion body */
+  .MuiAccordionDetails-root {
+    background: ${(props) => props.theme.body};
+    color: ${(props) => props.theme.text};
+
+    p {
+      font-size: 16px;
+      font-weight: 300;
+      text-align: initial;
+      color: inherit; /* FIX: prevents invisible text */
     }
 
-    .accordion__body {
-      padding: 0 0.625rem 0 0.625rem;
-      p {
-        font-size: 16px;
-        font-weight: 300;
-        text-align: initial;
-        margin: 0;
-      }
+    ul {
+      padding-left: 1.2rem;
     }
+
+    li {
+      color: inherit;
+    }
+      
   }
+
   div.faqbutton {
-    text-align: center; 
-    
+    text-align: center;
+
     button.faqbutton {
       margin-bottom: 1.25rem;
     }
   }
+
   .section-title {
     text-align: center;
+
     h1 {
       margin-bottom: 3.75rem;
       font-size: 40px;
       line-height: 3.125rem;
     }
+
     .search {
       margin-bottom: 3.125rem;
+
       input {
         font-size: 20px;
         width: 80%;
         padding: 1rem;
-        border: 1px solid #FFFFFF;
-        background-color: #F0F0F0;
+        border: 1px solid #ffffff;
+        background-color: #f0f0f0;
         border-radius: 1.25rem;
       }
     }
   }
+
   .askus_section {
     text-align: left;
+
     h2 {
       margin-bottom: 1rem;
     }
+
     p {
       margin-bottom: 2rem;
     }
+
     button {
       margin-bottom: 0.5rem;
-    }
-  }
-  @media only screen and (max-width: 912px) {
-    .reusecore__accordion {
-      margin-bottom: 3.125rem;
     }
   }
 
@@ -91,23 +127,13 @@ const FaqSectionWrapper = styled.section`
     .section-title {
       text-align: center;
     }
-    .reusecore__accordion {
-      text-align: left;
-    }
-    h2 {
-      text-align: left;
-    }
   }
-  
+
   @media only screen and (max-width: 480px) {
-    .accordion__item {
-      .accordion__header {
-        h5 {
-          font-size: 13px;
-          line-height: 21px;
-          padding-right: 1.6rem;
-        }
-      }
+    .MuiAccordionSummary-root h5 {
+      font-size: 13px;
+      line-height: 21px;
+      padding-right: 1.6rem;
     }
   }
 `;
